c7aef7acf74032cf1890287ed9c49cefc6b8972a
Mac <mac@MacdeMacBook-Air.local>
2023-06-29 11:07:16 +0800

    finished proj0

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author Kris
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -54,6 +54,7 @@ public class Model extends Observable {
 
     /** Return the number of squares on one side of the board.
      *  Used for testing. Should be deprecated and removed. */
+
     public int size() {
         return board.size();
     }
@@ -112,15 +113,115 @@ public class Model extends Observable {
 
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
-        // changed local variable to true.
+        // changed local variable to true.我的任务，让tile move
 
+        int size = this.size();
+        int empty_row, empty_col, key_row, key_col, key_value;
+
+        int secondKeyRow = size;
+        /** 4 4 2
+         *  4   2
+         *  4 4
+         *  4 4 4
+         *
+         *  */
+        this.board.setViewingPerspective(side);
+        //如果有相邻合并项, 检查可以合并的位置, 先合并还是先移动？先合并。从上向下检查，向上合并
+       for(int col = 0; col < size; col++) {
+           for(int row = size - 1; row >= 0; row--) {
+               empty_row = CheckFirstEmpty(row, col);// return first,empty_col
+               key_row =  CheckFirstValue(row, col);// return first key_col
+               if( empty_row != size && key_row != size && empty_row > key_row) {
+                   //直接移动到空白位置,这是第一种情况，空格在前面情况
+                   Tile t = this.board.tile(col, key_row);
+                   this.board.move(col, empty_row, t);
+                   changed = true;
+                   row += 1;//移动到空白位置后，再从空白位置为起点查找
+               }
+               else if(key_row != size)//  第二种情况，空格在后面
+               {
+                    secondKeyRow = findTheSameVal(key_row, col);//若有相同的值，返回对应的row
+                    if(secondKeyRow != size) {
+                        //情况是后面有相同的值, 进行合并,若没有，就继续向下检查
+                        this.score = this.score + (this.board.tile(col, secondKeyRow).value() * 2);
+                        Tile t = this.board.tile(col, secondKeyRow);
+                        this.board.move(col, key_row, t);
+                        //this.board.tile(col, secondKeyRow).move(col, key_row);
+                        changed = true;
+                    }
+               }
+           }
+       }
+        this.board.setViewingPerspective(Side.NORTH);
         checkGameOver();
         if (changed) {
             setChanged();
         }
         return changed;
     }
+    /** To find out the first empty row, if not, return size*/
+    private int CheckFirstEmpty(int row, int col) {
 
+        int size = this.size();
+        for(; row >= 0 ; row--) {
+            if(this.board.tile(col, row) == null) {
+                return row;
+            }
+        }
+        return size;
+    }
+    /**To find out the first valid row(has value), if not return size  */
+    private int CheckFirstValue(int row, int col) {
+
+        int size = this.size();
+        for(; row >= 0; row--) {
+            if(this.board.tile(col, row) != null) {
+                return row;
+            }
+        }
+        return size;
+    }
+    /** find the same value, return the corresponding row, if not found ,return size*/
+    private int findTheSameVal(int key_row, int col) {
+
+        int key_val = this.board.tile(col, key_row).value();
+        key_row -= 1;
+        for(; key_row >= 0; key_row -- ){
+            if(this.board.tile(col, key_row)!= null && this.board.tile(col, key_row).value() == key_val) {
+                return key_row;
+            }
+        }
+        return this.board.size();
+
+    }
+
+    /**To check one specific one col to see if it has one empty place, if find one,
+     * it will return the empty place promptly, if not find, return 0 , 它不让你移动多次，必须一次找到位置
+     **/
+    private int checkCol(int col, int row)
+    {
+        Tile t;
+        int empty_row = 0;
+        for(; row < this.size(); row++) {
+            if(this.board.tile(col, row) == null) {
+                empty_row = row;
+                return empty_row;
+            }
+        }
+        return empty_row;
+    }
+    /**检查某列中是否可以有合并的项, 并返回可以此列中最上面合并行的位置 */
+    private int CheckRowAdjacentValues(int col) {
+
+        int valid_mergering_row = 0;
+        for(int row = 0; row < this.board.size() - 1; row++) {
+
+            if(this.board.tile(col, row) != null && this.board.tile(col, row).value() == this.board.tile(col, row + 1).value() ){
+                valid_mergering_row = row + 1;
+            }
+        }
+        return valid_mergering_row;
+    }
     /** Checks if the game is over and sets the gameOver variable
      *  appropriately.
      */
@@ -137,7 +238,13 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+
+        int size = b.size();
+        for( int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if(b.tile(row, col) == null) return  true;
+            }
+        }
         return false;
     }
 
@@ -147,7 +254,14 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        //row是行，col是列
+        /** 思路是先固定行，再遍历列*/
+        int size = b.size();
+        for(int row = 0; row < size; row++) {
+            for(int col = 0; col < size; col++) {
+                if(b.tile(col, row) != null && b.tile(col, row).value() == MAX_PIECE) return true;
+            }
+        }
         return false;
     }
 
@@ -158,8 +272,28 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        if(emptySpaceExists(b)) return true;
+        if(checkAdjacentValues(b)) return true;
+
         return false;
+    }
+    public static boolean checkAdjacentValues(Board b) {
+        int size = b.size();
+        /** Check adjacent row行  tiles */
+        for(int col = 0; col < size; col++) {
+            for( int row = 0; row < size - 1; row++) {
+                if(b.tile(col, row).value() == b.tile(col, row + 1).value()) return true;
+            }
+        }
+        /** Check adjacent col列 tiles */
+        for(int row = 0; row < size; row++) {
+            for(int col = 0; col < size - 1; col ++) {
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value()) return true;
+            }
+        }
+
+        return false;
+
     }
 
 
